136f52cd2dff1179bd2871c7d9f60d1e2451470f
Adnansami691 <samiadnn691@gmail.com>
2025-02-05 22:50:39 +0600

    Your commit message

---
+++ codes/2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void greet(char* name) {
+    // Write code here
+    char n[] = "Hello, ";
+    scanf("%s", &name);
+    printf("%s", n, name);
+}
+int main() {
+    greet(char* name);
+}
