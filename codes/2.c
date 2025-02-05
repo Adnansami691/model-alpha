3d2d96fcd528979f678a31aa139eb21aeaa8ca4a
Adnansami691 <samiadnn691@gmail.com>
2025-02-05 23:47:52 +0600

    tried new

--- codes/2.c
+++ codes/2.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
-void greet(char* name) {
-    // Write code here
-    char n[] = "Hello, ";
-    scanf("%s", &name);
-    printf("%s", n, name);
-}
 int main() {
-    greet(char* name);
+    int day;
+    printf("enter day(1-7) : ");
+    scanf("%d", &day);
+    switch (day)
+    {
+    case 1 : printf("monday \n");
+        break;
+    case 2 : printf("tuesday \n");
+        break;
+    case 3 : printf("wednesday \n");
+        break;
+    case 4 : printf("thursday \n");
+        break;
+    case 5 : printf("friday \n");
+        break;
+    case 6 : printf("saturday \n");
+        break;
+    case 7 : printf("sunday \n");
+        break;
+    default: printf("not a valid day! \n");
+        break;
+    }
 }
