18642ab4a72a1c58f01fdefd5b35d6929fa2ef85
Adnansami691 <samiadnn691@gmail.com>
2025-02-06 14:00:51 +0600

    new challenges

---
+++ codes/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    float height = 1.82 * 1.82;
+    float weight = 72;
+    // Complete the code
+    float bmi = weight / height;
+    printf("%f", bmi);
+    return 0;
+}
+
+
+
+
+
+int main() {
+    float fahrenheit = 98.3;
+    // Complete the code
+    float cel = 98.3 - 32;
+    float res = cel / 1.8;
+    printf("%f", res);
+    return 0;
+}
+
+
+
+
+
+
+
